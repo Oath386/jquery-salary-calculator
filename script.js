0d6fa5105af54337e7983457ff08f143c106412e
Ivan Figueroa <ivanfigueroa386@gmail.com>
2020-02-01 13:50:03 -0600

    Second Commit

--- script.js
+++ script.js
@@ -2,33 +2,73 @@ console.log('test test test ');
 
 $(document).ready(onReady);
 
-let salaryCalculator = [];
+let employeeInfo = [];
 
 function onReady(){
     console.log('in onReady');
     $('#addSubmitButton').on('click', submit)
+    
 }//end on ready 
-
-
 
 function submit() {
     console.log('in Submit');
+
+    //select inputs by ID
+    //use their data in a new Info object 
+
     let newInfo={
         firstName: $('#firstIn').val(),
         lastName: $('#lastIn').val(),
-        ID: $('#idIn').val(),
+        id: $('#idIn').val(),
         title: $('#title').val(),
         annualSalary: $('#annualSalaryIn').val()
+    }
 
-       
+        $('#firstIn').val('');
+        $('#lastIn').val('');
+        $('#idIn').val('');
+        $('#annualSalaryIn').val('');
 
-  
+    //end New Info
+    //push new info object into salaryCalculator arrray 
 
+    employeeInfo.push(newInfo);
 
-
-    }//end New Info
+    displayEmployees();
 
 //select input by ID
     
     
 }//end submit function 
+
+function displayEmployees(){
+    console.log('in Display ');
+    //target an output element by ID
+    let el= $('#employeesOut')
+    //empty out element
+    el.empty();
+    //loop through salaryCalculator Array
+    for(let i=0; i<employeeInfo.length; i++){
+        el.append(`
+        <tr>
+        <td>${employeeInfo[i].firstName}</td>
+        <td>${employeeInfo[i].lastName}</td>
+        <td>${employeeInfo[i].id}</td>
+        <td>${employeeInfo[i].title}</td>
+        <td>${employeeInfo[i].annualSalary}</td>
+        `);
+    } //end displayEmployees
+
+
+
+}//end display Employees 
+
+function calculator(){
+    console.log('in Calculator');
+    for (let i = 0; i < array.length; i++) {
+        $('#annualSalaryIn').on('click', )
+        
+        
+    }
+    
+}
